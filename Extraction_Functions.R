213bf6b4b37e1694a056291a892490aa5f51354c
DavidOnadeko <donadeko@mit.edu>
2024-08-07 11:21:47 -0500

    Updated Code

--- Extraction_Functions.R
+++ Extraction_Functions.R
@@ -17,6 +17,22 @@ get_contrib_roles = function(contrib_nodes){
   return (dt)
 }
 
+get_contrib_roles_matrix = function(contrib_nodes){
+  #'Extract contributor information
+  #'
+  #'Takes in a vector of contrib nodes and extracts name, contributor type, and contributor role from it
+    tempmatrix <- matrix(data = NA, ncol = 3,nrow = 5000)
+    for (i in 1:length(contrib_nodes)){
+    contrib_node = contrib_nodes[i]
+    name = contrib_node %>% xml_child(search = "name") %>% xml_text()
+    contrib_type = contrib_node %>% xml_attr("contrib-type")
+    roles = paste(contrib_node %>% xml_find_all("role") %>% xml_text(), collapse = ', ')
+    tempmatrix[i,] <- c(name, contrib_type, roles)
+    }
+    tempmatrix <- tempmatrix[rowSums(is.na(tempmatrix)) != ncol(tempmatrix),]
+  return (tempmatrix)
+}
+
 
 
 # testfile = 'journal.pone.0254062.xml'
--- NewExtractionFIle.R
+++ NewExtractionFIle.R
@@ -9,24 +9,52 @@ ALLFILES = list.files(path = "./allofplos", pattern = NULL, all.files = FALSE,
 
 length(ALLFILES)
 
-Maindf  = tibble()
+# Maindf  = tibble()
+#
+# i = 0
+#
+# for(file in ALLFILES){
+#   i = i+1
+#
+#   filepath = file.path('./allofplos',file)
+#   xmlfront = read_xml(filepath) %>% xml_child(search = 'front')
+#   contrib_nodes = xml_find_all(xmlfront, "//contrib")
+#   df = get_contrib_roles(contrib_nodes)
+#   Maindf <- bind_rows(Maindf, df)
+#   if(i%%1000 == 0){
+#   print(paste(i,'thousand done'))
+#   }
+#
+# }
+#
+# View(Maindf)
+#
 
-i = 0
 
-for(file in ALLFILES[1:10000]){
-  i = i+1
+Mainmat <- matrix(ncol = 3)
 
+filesdone = 0
+filessaved = 0
+
+for(file in ALLFILES[]){
   filepath = file.path('./allofplos',file)
   xmlfront = read_xml(filepath) %>% xml_child(search = 'front')
   contrib_nodes = xml_find_all(xmlfront, "//contrib")
-  df = get_contrib_roles(contrib_nodes)
-  Maindf <- bind_rows(Maindf, df)
-  if(i%%1000 == 0){
-  print(paste(i,'thousand done'))
+  mat = get_contrib_roles_matrix(contrib_nodes)
+  Mainmat <- rbind(Mainmat, mat)
+  filesdone = filesdone+1
+  if(filesdone%%1000 == 0){
+    print(paste(filesdone,'files done','|',filessaved,'files saved'))
   }
-
+  if (filesdone%%30000 == 0){
+    filessaved = filessaved + 1
+    filename = paste(c('contrib_roles',filessaved,'.csv'),collapse = '')
+    matfilepath = file.path('./Contributor_roles_files',filename)
+    Mainmat <- Mainmat[rowSums(is.na(Mainmat)) != ncol(Mainmat),]
+    # write.csv(Mainmat,file=matfilepath)
+    # Mainmat <- matrix(ncol = 3)
+  }
 }
-
-View(Maindf)
-View(maindataframe)
+# write.csv(Mainmat,file='./Contributor_roles_files/contrib_roles14.csv')
+View(Mainmat)
 
