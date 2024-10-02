02a7268f2cc12646da6c25c5eddfe3bbb9abfaf9
DavidOnadeko <donadeko@mit.edu>
2024-10-02 05:50:01 -0400

    Updating GenderImputation

--- GenderImputation.R
+++ GenderImputation.R
@@ -6,23 +6,85 @@ library(rplos)
 library(opengender)
 library(stringr)
 ImportedData = readRDS("./Data(rds files)/Contrib_dataV7.rds")
+ImportedData2 = readRDS("./Data(rds files)/Reviewer_names.rds")
 
-View(ImportedData)
+# View(ImportedData)
 
-test = ImportedData %>% unnest(data)
+Contribs = ImportedData %>% unnest(data)
 
-names = test[,c('Given Name', 'Surname')] %>% rename(given = 'Given Name')
+ContribsEditedTibble = Contribs[,"Given Name"] %>% rename(given = 'Given Name')%>% mutate(given = trimws(given)) %>% mutate(given = word(given, 1))
 
-View(names)
+test = opengender::add_gender_predictions(ContribsEditedTibble[1,], dicts = c("wgen2"))
 
-# Surnames = names[,'Surname']
-G_names = names[, 'given'] %>% mutate(given = word(given, 1))
+#Reviewer Information
 
-# View(Surnames)
-View(G_names)
+Reviewers = ImportedData2 %>% unnest(Reviewers)
 
-Given_imputation = opengender::add_gender_predictions(G_names, dicts = c("wgen2"))
+ReviewersEditedTibble = Reviewers[,"Reviewers"] %>% rename(given = 'Reviewers')%>% mutate(given = trimws(given)) %>% mutate(given = word(given, 1))
+View(Reviewers)
+View(ReviewersEditedTibble)
 
-View(Given_imputation)
 
-gender_mean(Given_imputation$og_pr_F)
+Given_imputation = opengender::add_gender_predictions(ReviewersEditedTibble[1:10000,], dicts = c("wgen2"))
+saveRDS(Given_imputation, file = "./Data(rds files)/ReviewersImputationPt1.rds")
+
+Given_imputation = opengender::add_gender_predictions(ReviewersEditedTibble[10001:20000,], dicts = c("wgen2"))
+saveRDS(Given_imputation, file = "./Data(rds files)/ReviewersImputationPt2.rds")
+
+Given_imputation = opengender::add_gender_predictions(ReviewersEditedTibble[20001:30000,], dicts = c("wgen2"))
+saveRDS(Given_imputation, file = "./Data(rds files)/ReviewersImputationPt3.rds")
+
+Given_imputation = opengender::add_gender_predictions(ReviewersEditedTibble[30001:31176,], dicts = c("wgen2"))
+saveRDS(Given_imputation, file = "./Data(rds files)/ReviewersImputationPt4.rds")
+# x = rep(0,31176)
+
+a = readRDS("./Data(rds files)/ReviewersImputationPt1.rds")
+b = readRDS("./Data(rds files)/ReviewersImputationPt2.rds")
+c = readRDS("./Data(rds files)/ReviewersImputationPt3.rds")
+d = readRDS("./Data(rds files)/ReviewersImputationPt4.rds")
+x = bind_rows(a,b,c,d)
+View(x)
+
+ReviewersWithImputation = Reviewers %>% bind_cols(x)
+View(ReviewersWithImputation)
+saveRDS(ReviewersWithImputation, file = "./Data(rds files)/ReviewersImputation1.rds")
+
+
+
+
+
+#Contrib Information
+
+ContribImputation = uncount(tibble(test), 2824282)
+View(ContribImputation)
+
+#For loop to sequentially run the imputation over the file without running out of memory(hopefully)
+for (i in 1:283){
+  print(i)
+  if (i == 283){
+    Given_imputation = opengender::add_gender_predictions(ContribsEditedTibble[2820001:2824282,], dicts = c("wgen2"))
+    ContribImputation[2820001:2824282,] = Given_imputation
+  }else{
+    Given_imputation = opengender::add_gender_predictions(ContribsEditedTibble[((i-1)*10000 + 1):(i* 10000),], dicts = c("wgen2"))
+    ContribImputation[((i-1)*10000 + 1):(i* 10000),] = Given_imputation
+  }
+}
+
+saveRDS(ContribImputation, file = "./Data(rds files)/ContribImputation1.rds")
+
+ContribWithImputation = Contribs %>% bind_cols(ContribImputation)
+
+saveRDS(ContribWithImputation, file = "./Data(rds files)/ContribWithImputation1.rds")
+
+
+View(ContribWithImputation[20000,])
+
+# View(Given_imputation)
+
+#Funding Imputation
+
+Funding = readRDS("./Data(rds files)/Funding_Info.rds") %>% unnest(info)
+View(Funding)
+
+#
+# gender_mean(Given_imputation$og_pr_F)
